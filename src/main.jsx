69a72233bd3022136f42514517203cfc2cda7572
Paúl Guevara <pauljgl2008@gmail.com>
2024-12-30 08:59:23 -0500

    feat: JournalApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles.css'
+import { JournalApp } from './JournalApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <JournalApp />
   </StrictMode>,
 )
